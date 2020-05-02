eb596db3a2af3e4c26e06f8369cf9e79c1391597
George Kary <geogkary@gmail.com>
2020-05-02 14:43:46 +0300

    Update Router.php

---
+++ versions/boilerplate/Router.php
@@ -0,0 +1,14 @@
+<?php
+
+class Router
+{
+
+    public static $keys = array();
+
+    public static $endpoints = array();
+
+    public static $errors = array();
+
+    public static $db = array();
+
+}
