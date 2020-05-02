0ef5db5512ce56e1d368235389cd0f095fb97890
George Kary <geogkary@gmail.com>
2020-05-02 15:30:29 +0300

    Cleanup

--- engine/index.php
+++ engine/index.php
@@ -9,7 +9,7 @@
 header('Content-Type: application/json');
 
 if (!class_exists('Flight') && file_exists('libraries/flight/Flight.php')) {
-    require 'libraries/Flight.php';
+    require 'libraries/flight/Flight.php';
 } else {
     echo json_encode(array(
         'status' => '500',
