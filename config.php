76c2c89c0d4079cc52d09ad9ec15b6d4118f2a27
George Kary <geogkary@gmail.com>
2020-05-02 18:19:54 +0300

    Update config.php

--- config.php
+++ config.php
@@ -8,3 +8,4 @@
 
 define('BZ_LIST_ENDPOINTS', true); // serve a list of your endpoints on home requests
 define('BZ_DEBUG', false); // override the 500 error with the exception info
+define('BZ_AUTOLOAD_LIBRARIES', false);
